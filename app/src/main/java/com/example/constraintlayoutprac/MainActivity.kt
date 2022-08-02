495c13baf4a84aa960772f0d104b2b065ece6312
anreh <anurehka@gmail.com>
2022-08-02 10:41:16 +0800

    style res

--- app/src/main/java/com/example/constraintlayoutprac/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayoutprac/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 
@@ -21,10 +22,13 @@ class MainActivity : AppCompatActivity() {
         val boxFourText = findViewById<TextView>(R.id.box4)
         val boxFiveText = findViewById<TextView>(R.id.box5)
 
+        val redButton = findViewById<Button>(R.id.button1)
+        val greenButton = findViewById<Button>(R.id.button2)
+        val yellowButton = findViewById<Button>(R.id.button3)
 
         val clickableViews: List<View> =
             listOf(
-                boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText
+                boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText,redButton,greenButton,yellowButton
             )
 
         for (item in clickableViews) {
@@ -34,11 +38,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeColoured(view: View) {
         when (view.id) {
-            R.id.box1 ->view.setBackgroundColor(Color.YELLOW)
-            R.id.box2 ->view.setBackgroundColor(Color.CYAN)
-            R.id.box3 ->view.setBackgroundColor(Color.LTGRAY)
-            R.id.box4 ->view.setBackgroundColor(Color.MAGENTA)
-            R.id.box5 ->view.setBackgroundColor(Color.RED)
+            R.id.box1 -> view.setBackgroundColor(Color.YELLOW)
+            R.id.box2 -> view.setBackgroundColor(Color.CYAN)
+            R.id.box3 -> view.setBackgroundColor(Color.LTGRAY)
+            R.id.box4 -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box5 -> view.setBackgroundColor(Color.RED)
+            // Boxes using custom colors for background
+            R.id.button1 -> boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.button2 ->  view.setBackgroundResource(R.color.my_yellow)
+            R.id.button3 ->  view.setBackgroundResource(R.color.my_green)
+
+
             else -> view.setBackgroundColor(Color.BLACK)
         }
     }
